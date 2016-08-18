88f5239fb40329197610be47f6b987762f1cf977
weimingtom <weimingtom@qq.com>
2016-08-19 02:30:50 +0800

    my pc

--- lib/console.bat
+++ lib/console.bat
@@ -1,3 +1,6 @@
+@set path=E:\Ruby187\bin;%path%
 @set path=E:\Ruby187\lib\ruby\gems\1.8\gems\rubysdl-mswin32-1.8-2.1.1.1\dll;%path%
+@set path=D:\Ruby187\bin;%path%
+@set path=D:\Ruby187\lib\ruby\gems\1.8\gems\rubysdl-mswin32-1.8-2.1.1.1\dll;%path%
 @cmd
 
